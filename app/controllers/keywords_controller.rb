d3a00bec2f3e6c691484241d539d604b1bc9bd68
Varun Dubey <dvarun@hotmail.com>
2015-11-14 00:58:40 +0700

    init heroku

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -14,41 +14,85 @@ class KeywordsController < ApplicationController
 
  def create
   myfile = params[:file]
-  @words = []
   CSV.foreach(myfile.path) do |row|
-   @words << row.shift.strip
+   # @keyword = Keyword.new
+   # @keyword.word = row.shift.strip
+   # @keyword.user_id = current_user.id
+   # @keyword.save
+   # @words << row.shift.strip
+  end
+  @words = Keyword.where(user_id: current_user.id).limit(10)
+
+  count = 0
+  @all_links = []
+  @top_adwords = []
+  @top_adwords_url = []
+  @right_adwords = []
+  @right_adwords_url = []
+  @all_non_adwords = []
+  @all_non_adwords_url = []
+  for keyword in @words
+   # begin
+   #  page = open "http://www.google.com/search?q=#{keyword.word.gsub! ' ','+'}"
+   #   page = open "http://www.google.com/search?q=Best+Vps+Hosting"
+   # rescue Errno::ECONNRESET => e
+   #  count += 1
+   #  retry unless count > 10
+   # end
+   #
+  end
+  page = open "https://www.google.com/search?q=best+vps+Hosting"
+  doc = Nokogiri::HTML page
+  doc.xpath('//cite').each do |node| #all_links
+   @all_links << node.text
   end
 
-  @keyword = Keyword.new
-  @keyword.word = @words
-  @keyword.user_id = current_user.id
-  @keyword.save
+  doc.xpath('//div[@id="tvcap"]//li[@class="ads-ad"]/h3/a').each do |node| #top_adwords
+   @top_adwords << node.text
+  end
+
+  doc.xpath('//div[@id="tvcap"]//li[@class="ads-ad"]//cite').each do |node| #top_adwords_url
+   @top_adwords_url << node.text
+  end
+
+  doc.xpath('//div[@id="rhs_block"]//li[@class="ads-ad"]/h3/a').each do |node| #right_adwords
+   @right_adwords << node.text
+  end
+
+  doc.xpath('//div[@id="rhs_block"]//li[@class="ads-ad"]//cite').each do |node| #right_adwords_url
+   @right_adwords_url << node.text
+  end
+
+  @total_adwords = @top_adwords.count +  @right_adwords.count
+
+  doc.xpath('//div[@id="res"]//h3').each do |node| #all_non_adwords
+   @all_non_adwords << node.text
+  end
+
+  doc.xpath('//div[@id="res"]//cite').each do |node| #all_non_adwords_url
+   @all_non_adwords_url << node.text
+  end
 
 
-  page = open "http://www.google.com/search?q=best+vps+hosting"
-  doc = Nokogiri::HTML page
-  @search = []
-  # doc.xpath('//li[@class="ads-ad"]/h3').each do |node|
-  #  @search << node.text
-  # end
-  @search << doc.search('//div[@id="tvcap"]//li[@class="ads-ad"]/h3/a').to_s #top_adwords
-  @search << doc.search('//div[@id="tvcap"]//li[@class="ads-ad"]//cite').to_s #top_adwords_url
 
-  @search << doc.search('//div[@id="rhs_block"]//li[@class="ads-ad"]/h3/a').to_s #right_adwords
-  @search << doc.search('//div[@id="rhs_block"]//li[@class="ads-ad"]//cite').to_s #right_adwords
-
-  @search << doc.search('//li[@class="ads-ad"]/h3/a').to_s #all_adwords
-  @search << doc.search('//li[@class="ads-ad"]//cite').to_s #all_adwords_url
-
-  @search << doc.search('//div[@id="res"]//h3').to_s #all_non_adwords
-  @search << doc.search('//div[@id="res"]//cite').to_s #all_non_adwords_url
-
-  #@search << doc.search('//div[@id="res"]//h3/a/@href').to_s #all_non_adwords_url
-  @search << doc.search('//cite').to_s #all_links
-
-  @search << doc.search('//div[@id="resultStats"]/text()').to_s #total_result_stats
-
-  @search << doc.search('//*')
+  # @search << doc.search('//div[@id="tvcap"]//li[@class="ads-ad"]/h3/a').to_s #top_adwords
+  # @search << doc.search('//div[@id="tvcap"]//li[@class="ads-ad"]//cite').to_s #top_adwords_url
+  #
+  # @search << doc.search('//div[@id="rhs_block"]//li[@class="ads-ad"]/h3/a').to_s #right_adwords
+  # @search << doc.search('//div[@id="rhs_block"]//li[@class="ads-ad"]//cite').to_s #right_adwords_url
+  #
+  # @search << doc.search('//li[@class="ads-ad"]/h3/a').to_s #all_adwords
+  # @search << doc.search('//li[@class="ads-ad"]//cite').to_s #all_adwords_url
+  #
+  # @search << doc.search('//div[@id="res"]//h3').to_s #all_non_adwords
+  # @search << doc.search('//div[@id="res"]//cite').to_s #all_non_adwords_url
+  #
+  # #@search << doc.search('//div[@id="res"]//cite').to_s #all_non_adwords_url
+  # @search << doc.search('//cite').to_s #all_links
+  #
+  # @search << doc.search('//div[@id="resultStats"]/text()').to_s
+  #
+  # @search << doc.search('//*')
 
  end
 
