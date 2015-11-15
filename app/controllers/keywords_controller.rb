d41850fd721cd76543c11e1ae9b302fd9d70d840
Varun Dubey <dvarun@hotmail.com>
2015-11-15 16:00:59 +0700

    fixed something

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -42,9 +42,9 @@ class KeywordsController < ApplicationController
 
     search_key = keyword.word.gsub! ' ','+'
     if search_key.nil?
-     page = open "https://www.google.com/search?q=#{keyword.word}"
+     page = open "http://www.google.com/search?q=#{keyword.word}"
     else
-     page = open "https://www.google.com/search?q=#{search_key}"
+     page = open "http://www.google.com/search?q=#{search_key}"
     end
     #page = open "http://www.google.com/search?q=hello"
     doc = Nokogiri::HTML page
